d7af06e17602163eb7867fc2021582dd9f6641f3
jufrancopy <jucfra23@gmail.com>
2023-01-21 17:11:15 -0300

    Correcion de Title

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title') - Ipu Paraguay</title>
+    <title>@yield('title') - GMRE</title>
     <meta name="viewport" content="width=devide-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="routeName" content="{{ Route::currentRouteName() }}">
